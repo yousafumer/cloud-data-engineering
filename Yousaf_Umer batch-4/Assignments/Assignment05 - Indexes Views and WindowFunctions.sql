e978eaf3b181af1b9f0e5b7a2d574d89907cae68
yousafumer <yousafumer128@gmail.com>
2026-06-07 18:00:47 +0500

    add assignment 5

--- Yousaf_Umer batch-4/Assignments/Assignment05 - Indexes Views and WindowFunctions.sql
+++ Yousaf_Umer batch-4/Assignments/Assignment05 - Indexes Views and WindowFunctions.sql
@@ -24,7 +24,11 @@
 -- This query is slow. Create an appropriate index to fix it.
 -- Then run the query to confirm it returns results correctly.
 
+Create nonclustered index brand_ind1x on production.products (brand_id)
 
+Select product_id,  product_name, List_price
+From production.products
+where brand_id = 3
 
 -- Q2.
 -- The finance team runs a monthly report that filters orders
@@ -35,6 +39,12 @@
 --   WHERE order_date BETWEEN '2018-01-01' AND '2018-06-30';
 --
 -- Create an index to make this query more efficient.
+
+Create nonclustered index date_indx on sales.orders(order_date)
+
+select order_id, customer_id, order_date
+From sales.orders
+where order_date between '2018-01-01' And '2018-06-30'
 
 
 
@@ -51,6 +61,25 @@
 --   (not a number — use 1=Pending, 2=Processing).
 -- After creating it, query the view to see today's workload.
 
+create view order_detail
+as
+	Select order_id, first_name+'_'+last_name as full_name, phone , email, o.order_date , 
+
+	case when o.order_status = 1 then 'pending'
+		when o.order_status = 2 then'processing'
+
+		end sts
+		From sales.customers c
+		inner join sales.orders o
+		on c.customer_id = o.customer_id
+		where o.order_status in (1,2)
+
+Select * from order_detail
+
+
+		
+
+	
 
 
 -- Q4.
@@ -61,6 +90,23 @@
 -- After creating it, query the view to find all products
 -- that have fewer than 3 units remaining in any store.
 
+create view stock_monitor 
+as
+	Select s.store_name , st.quantity, p.product_name , b.brand_name, c.category_name
+	From sales.stores s
+	left join production.stocks st
+	on s.store_id = st.store_id
+	inner join production.products p
+	on st.product_id = p.product_id
+	left join production.brands b
+	on  p.brand_id = b.brand_id
+	left join production.categories c
+	on p.category_id = c.category_id
+
+
+
+select  * from stock_monitor
+where quantity <=3
 
 
 -- ============================================================
@@ -73,6 +119,29 @@
 -- Show store_id, product_id, total_quantity, and their rank within the store.
 -- Return only rank 1 and rank 2 for each store.
 
+with store_total as(
+
+	Select o.store_id, oi.product_id ,  sum(oi.quantity) as product_count 
+
+	From sales.orders o
+	inner join sales.order_items oi
+	on o.order_id = oi.order_id
+	group by o.store_id, oi.product_id ),
+
+ranked as (
+	 Select store_id, product_id ,   product_count ,
+	 Rank () over (partition by store_id order by product_count desc) as rn
+	 From store_total )
+
+Select store_id, product_id, product_count ,rn
+from ranked
+where rn<=2
+order by store_id
+	 
+
+
+
+
 
 
 -- Q6.
@@ -81,7 +150,11 @@
 -- Show category_id, product_name, list_price, and their price rank
 -- within the category.
 -- Return only the products ranked 2nd in their category.
-
+select * from (
+	Select  category_id, product_name, list_price,
+	Dense_rank () over(partition by category_id order by list_price desc) as rn
+	From production.products ) as ranked
+	where rn = 2
 
 
 -- Q7.
@@ -93,25 +166,31 @@
 --
 -- Run this setup first:
 --
--- CREATE TABLE test_customers (
---     customer_id  INT,
---     first_name   VARCHAR(50),
---     last_name    VARCHAR(50),
---     phone        VARCHAR(20),
---     city         VARCHAR(50)
--- );
+ CREATE TABLE test_customers (
+     customer_id  INT,
+     first_name   VARCHAR(50),
+     last_name    VARCHAR(50),
+     phone        VARCHAR(20),
+     city         VARCHAR(50)
+ );
 --
--- INSERT INTO test_customers VALUES
---     (1,  'Ali',    'Khan',    '0300-1111111', 'Karachi'),
---     (2,  'Sara',   'Ahmed',   '0321-2222222', 'Lahore'),
---     (3,  'Ali',    'Khan',    '0300-1111111', 'Karachi'),   -- duplicate of 1
---     (4,  'Usman',  'Malik',   '0333-3333333', 'Islamabad'),
---     (5,  'Sara',   'Ahmed',   '0321-2222222', 'Lahore'),   -- duplicate of 2
---     (6,  'Sara',   'Ahmed',   '0321-2222222', 'Lahore'),   -- 3rd copy of 2
---     (7,  'Hina',   'Raza',    '0312-4444444', 'Peshawar');
---
--- Now write your query to find the duplicate rows.
+ INSERT INTO test_customers VALUES
+     (1,  'Ali',    'Khan',    '0300-1111111', 'Karachi'),
+     (2,  'Sara',   'Ahmed',   '0321-2222222', 'Lahore'),
+     (3,  'Ali',    'Khan',    '0300-1111111', 'Karachi'),   -- duplicate of 1
+     (4,  'Usman',  'Malik',   '0333-3333333', 'Islamabad'),
+     (5,  'Sara',   'Ahmed',   '0321-2222222', 'Lahore'),   -- duplicate of 2
+     (6,  'Sara',   'Ahmed',   '0321-2222222', 'Lahore'),   -- 3rd copy of 2
+     (7,  'Hina',   'Raza',    '0312-4444444', 'Peshawar');
 
+-- Now write your query to find the duplicate rows.
+Select * from (
+	Select customer_id,first_name, last_name, phone , 
+		ROW_NUMBER () over (partition by first_name, last_name, phone order by customer_id ) as rw
+	From test_customers
+) as dp
+where rw >=2
+order by rw
 
 
 -- ============================================================
@@ -126,6 +205,23 @@
 -- Net sales = SUM( quantity * list_price * (1 - discount) )
 
 
+select 
+	months, net_sales,
+	lag(net_sales,1) over (order by months) as previous_month_sales,
+	net_Sales-lag(net_sales,1) over (order by months) as diffrence
+
+	
+	from (
+		Select month(o.order_date) as months, sum(oi.quantity * oi.list_price * (1 - oi.discount)) as net_Sales
+		from sales.orders o
+		inner join sales.order_items oi
+		on o.order_id = oi.order_id  
+		where year(order_date) = '2017'
+		group by month(order_date )) as result
+
+
+
+ 
 
 -- Q9.
 -- The product team wants to see each product's price compared to
@@ -133,6 +229,14 @@
 -- Show product_name, list_price, and the next lower price
 -- in the same category.
 -- Sort by category_id and list_price descending.
+
+
+select category_id, product_name, list_price,
+	lead(list_price,1) over (partition by category_id order by list_price desc) as next_lower_price
+from production.products
+order by category_id, list_price desc
+
+
 
 
 
@@ -143,6 +247,18 @@
 -- Replace any missing phone with their email address instead.
 -- If both are missing, show 'No Contact Info'.
 -- Sort by last_name, first_name.
+
+select 
+	first_name + ' ' + last_name as full_name,
+    phone,
+    email,
+	case 
+	when phone is null and email is null then 'No Contact Info'
+	when  phone is null then email
+	else phone
+	end as new_phone
+	from sales.customers
+	order by last_name, first_name
 
 
 
